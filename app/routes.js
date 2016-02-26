8b997b126b79f0bd2f177457b6794e1e19d3743b
Valentine Nwachukwu <valentine.nwachukwu@boeing.com>
2016-02-25 20:23:46 -0600

    finished integrating angular

--- app/routes.js
+++ app/routes.js
@@ -42,10 +42,10 @@ module.exports = function (app, passport) {
     });
 
     // send to facebook to do the authentication
-    app.route('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
+    app.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
 
     // handle the callback after facebook has authenticated the user
-    app.route('/auth/facebook/callback',
+    app.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
             successRedirect: '/profile',
             failureRedirect: '/'
--- public/js/services/AuthenticationFactory.js
+++ public/js/services/AuthenticationFactory.js
@@ -1,38 +1,31 @@
-app.factory('AuthenticationFactory', ['$rootScope', '$state', '$sessionStorage', '$http',
-function($rootScope, $state, $sessionStorage, $http, $auth) {
-  var factory = {};
+app.factory('AuthenticationFactory', ['$rootScope', '$state', '$sessionStorage', '$window',
+    function ($rootScope, $state, $sessionStorage, $window) {
+        var factory = {};
 
 
-    factory.login = function () {
-      console.log("Login");
-        $auth.authenticate(factory.user.platform).then(function() {
-          $sessionStorage[factory.user.platform] =
-          console.log("Login Success", $auth);
-          $state.go("default");
-        })
-        .catch(function(error) {
-          if (error.error) {
-            // Popup error - invalid redirect_uri, pressed cancel button, etc.
-            console.error(error.error);
-          } else if (error.data) {
-            // HTTP response error from server
-            console.error(error.data.message, error.status);
-          } else {
-            console.error(error);
-          }
-        });;
-    };
-    factory.logout = function () {
+        factory.login = function () {
+            var url = '/auth/' + factory.user.platform;
 
-    };
-    factory.user = {
-      userName: '',
-      password: '',
-      platform: null,
-      isLoggedIn: function () {
-        return false;
-      }
-    };
+            console.log(url);
+            if ($state.previous && $state.previous.href) {
+                url += '?redirect_to=' + encodeURIComponent($state.previous.href);
+            }
 
-  return factory;
-}]);
+            // Effectively call OAuth authentication route:
+            $window.location.href = url;
+        };
+
+        factory.logout = function () {
+
+        };
+        factory.user = {
+            userName: '',
+            password: '',
+            platform: null,
+            isLoggedIn: function () {
+                return false;
+            }
+        };
+
+        return factory;
+    }]);
