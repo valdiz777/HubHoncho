3e54a34ddea12b0d84cab913316607e622a7d6d0
Valentine Nwachukwu <Valentine Nwachukwu>
2016-05-15 15:00:24 -0500

    added instagram support

---
+++ server/accounts.jsx
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'instagram'
+});
+ServiceConfiguration.configurations.insert({
+    service: 'instagram',
+    scope: 'basic',
+    clientId: '7b297ccd426a4e4aa0e7a1b123d55b91',
+    secret: 'c1a300a2546f4b0cbb8bd7a8e6744bbc'
+});
