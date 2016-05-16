e09638c9fb495a1ca3314473af355444b15b222d
Valentine Nwachukwu <Valentine Nwachukwu>
2016-05-15 22:27:09 -0500

    added meteor cors support

--- server/init.jsx
+++ server/init.jsx
@@ -3,3 +3,9 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+// Listen to incoming HTTP requests, can only be used on the server
+WebApp.rawConnectHandlers.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return next();
+});
