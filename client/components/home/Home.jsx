322e281416741b4d1b764a728606de51fba6cd02
Valentine Nwachukwu <Valentine Nwachukwu>
2016-05-15 22:42:40 -0500

    added welcome message to homepage

--- client/components/home/Home.jsx
+++ client/components/home/Home.jsx
@@ -1,6 +1,28 @@
 import React, {Component} from 'react';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
-export default class Home extends Component {
+export default class Home extends TrackerReact(Component) {
+    constructor() {
+        super();
+
+        this.state = {
+            subscription: {
+                resolutions: Meteor.subscribe("userBooks")
+            }
+        }
+    }
+
+    componentWillUnmount() {
+        this.state.subscription.resolutions.stop();
+    }
+
+    isUserLoggedOn() {
+        if (!Meteor.userId()) {
+            return false;
+        }
+        return true;
+    }
+
     setVar() {
         Session.set('Meteor.loginButtons.dropdownVisible', true);
     }
@@ -11,7 +33,9 @@ export default class Home extends Component {
                 <h1>HubHoncho</h1>
                 <p>Hubhoncho is a unifying social media platform that provides the convenience and flexibity social
                     media connection....blah blah blah. </p>
-                <button onClick={this.setVar}>Sign Up</button>
+                {(this.isUserLoggedOn() === true) ? <h1>Welcome back {Meteor.userId()}</h1> :
+                    <button onClick={this.setVar}>Sign Up</button>
+                }
             </div>
         )
     }
